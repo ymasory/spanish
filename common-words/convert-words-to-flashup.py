9053ca54ca420e10c1299042ab885d9c54cc776f
Yuvi Masory <ymasory@gmail.com>
2011-08-31 23:03:49 -0400

    downloading file

--- common-words/convert-words-to-flashup.py
+++ common-words/convert-words-to-flashup.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
 #
-# Convert into Flashup format the word list from
-# Copy and paste the words in http://wordsgalore.com/wordsgalore/languages/spanish/spanish1000.html.
-# The word list has been hand-modified with zz's to demarcate vocab items with
-# multiple items.
-#
-# Usage: ./convert-words-to-flashup.py /path/to/words.txt
+# Usage: ./convert-words-to-flashup.py
 # Output: words.flashup
 
 import os
 import sys
+import tempfile as tempfilemodule
+from urllib.request import urlopen
 
 def main():
-    infile = os.path.abspath(sys.argv[1])
     LF = '\n'
+    url = ('http://wordsgalore.com/wordsgalore/languages/spanish/' +
+           'spanish1000.html' )
     header = '#GRAMMAR 1\n#TOPLEFT Common Spanish Words'
-    tmpfile =  infile + '.tmp'
-    infiledir, infilename = os.path.split(infile)
-    outfile = infiledir + os.sep + os.path.splitext(infilename)[0] + '.flashup'
+    tmpfile =  tempfilemodule.mkstemp()[1]
+    outfile = 'words.txt'
 
-    alldefs = set()
-    with open(infile) as lines:
-        with open(tmpfile, 'w') as out:
-            for line in lines:
-                if ' zz ' in line:
-                    parts = line.split(' zz ')
-                else:
-                    parts = line.split(' ')
-                out.write(parts[0] + '\t' + ' '.join(parts[1:]))
+    html = urlopen(url).read()
+    print(html)
+    sys.exit()
 
     with open(tmpfile) as lines:
         with open(outfile, 'w') as out:
