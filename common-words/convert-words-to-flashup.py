3fe161732fe876d5a85fcda40f6694fca03b58ff
Yuvi Masory <ymasory@gmail.com>
2011-09-01 18:22:29 -0400

    combine mp3 files

--- common-words/convert-words-to-flashup.py
+++ common-words/convert-words-to-flashup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #
 # Usage: ./convert-words-to-flashup.py
-# Output: words.flashup
+# Output: spanish-words.flashup, spanish-words-*.mp3
 
 from html.parser import HTMLParser
 import io
@@ -10,6 +10,7 @@ import os.path
 import sys
 import time
 from urllib.request import urlopen
+import yutil
 
 LF = '\n'
 url = 'http://wordsgalore.com/wordsgalore/languages/spanish/spanish1000.html'
@@ -51,6 +52,7 @@ class MyParser(HTMLParser):
 
 
 def main():
+    outputbase = 'spanish-words'
     with urlopen(url) as handle:
         html = handle.read()
         html = html.decode('windows-1252')
@@ -59,7 +61,7 @@ def main():
     parser.feed(html)
 
     header = ('#GRAMMAR 1' + LF + '#TOPLEFT Common Spanish Words' + LF * 2)
-    with open('spanish-words.flashup', 'w', encoding='utf-8') as out:
+    with open(outputbase + '.flashup', 'w', encoding='utf-8') as out:
         out.write(header)
 
         for mp3url, vocab, defn in parser.items:
@@ -72,14 +74,38 @@ def main():
     mp3dir = 'mp3s'
     if not os.path.exists(mp3dir):
         os.mkdir(mp3dir)
+    outpaths = []
     for i, mp3url in enumerate(mp3urls):
         fname = mp3dir + os.sep + os.path.basename(mp3url)
+        outpaths.append(fname)
         with open(fname, 'wb') as out:
             with urlopen(mp3url) as handle:
                 out.write(handle.read())
                 print('downloaded ' + str(i + 1) + '/' +
                       str(len(mp3urls) + 1) + ': ' + mp3url)
                 time.sleep(5) #being friendly
+
+    wrap = 'mp3wrap'
+    num = 0
+    suffix = 0
+    for i in range(len(outpaths) - 1):
+        if num % 100 == 0:
+            incr = True
+            suffix += 1
+        else:
+            incr = False
+        if incr:
+            flags = []
+        else:
+            flags = ['-a']
+        desired_name_noext = outputbase + '-' + str(suffix)
+        desired_name = desired_name_noext + '.mp3'
+        cli = [wrap] + flags + [desired_name] + outpaths[i:i+2]
+        print(cli)
+        yutil.carefulcall(cli)
+        if incr:
+            os.rename(desired_name_noext + '_MP3WRAP.mp3', desired_name)
+        num += 1
 
 
 if __name__ == '__main__':
